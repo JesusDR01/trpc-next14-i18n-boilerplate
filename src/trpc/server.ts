196f0e1d28b4aa90dcf7bc5b1752c7b4dd7a8ae7
JesusDR01 <jesus20.11@hotmail.es>
2023-11-19 01:35:11 +0100

    fix: unused code & prisma

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -17,7 +17,6 @@ export const api = createTRPCProxyClient<AppRouter>({
         (op.direction === "down" && op.result instanceof Error),
     }),
     unstable_httpBatchStreamLink({
-      // fetch: fetchPonyfill().fetch,
 
       url: getUrl(),
       headers() {
@@ -29,33 +28,3 @@ export const api = createTRPCProxyClient<AppRouter>({
     }),
   ],
 });
-
-
-// createTRPCProxyClient<AppRouter>({
-//   transformer,
-//   links: [
-//     devtoolsLink({
-//       enabled: process.env.NODE_ENV === 'development',
-//     }),
-//     splitLink({
-//       condition(op) {
-//         // check for context property `skipBatch`
-//         return op.context.skipBatch === true
-//       },
-//       // when condition is true, use normal request
-//       true: httpLink({
-//         // vercel issue with fetch undici
-//         fetch: fetchPonyfill().fetch,
-//         url: `${getBaseUrl()}/api/trpc`,
-//       }),
-//       // when condition is false, use batching
-//       false: httpBatchLink({
-//         fetch: fetchPonyfill().fetch,
-//         url: `${getBaseUrl()}/api/trpc`,
-//       }),
-//     }),
-//     loggerLink({
-//       enabled: (opts) => opts.direction === 'down' && opts.result instanceof Error,
-//     }),
-//   ],
-// })
